75956025e06dca28961c8de2cf27c86e6a8bfd35
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-02-07 14:26:33 +0530

    first commit

--- array.java
+++ array.java
@@ -1,16 +1,54 @@
-public class array {
-    public static void main(String[] args) {
-        int a[]=new int[5];
-        a[0]=12;
-        a[1]=34;
-        a[2]=56;
-        a[3]=78;
-        a[4]=90;
-        System.out.println("lenght of the array is "+a.length);
-        System.out.println("printing the element of the array.");
-        for(int i=0;i<a.length;i++)
-        System.out.println(a[i]);
+import java.util.Scanner;
+// public class array {
+//     public static void main(String[] args) {
+//         int a[]=new int[5];
+//         a[0]=12;
+//         a[1]=34;
+//         a[2]=56;
+//         a[3]=78;
+//         a[4]=90;
+//         System.out.println("lenght of the array is "+a.length);
+//         System.out.println("printing the element of the array.");
+//         for(int i=0;i<a.length;i++)
+//         System.out.println(a[i]);
         
-    }
+//     }
     
-}
+// }
+
+
+// public class array {
+// public static void main(String[] args) {
+//     int n;
+//     Scanner ob=new Scanner(System.in);
+//     System.out.println("Enter the number of the element:");
+//     n=ob.nextInt();
+//     int a[]=new int[n];
+
+//     System.out.println("Enter"+n+"element of array");
+//     for(int i=0;i<a.length;i++)
+//     a[i]=ob.nextInt();
+//     System.out.println("printing the element of array");
+//     for(int i=0;i<a.length;i++)
+//     System.out.println(a[i]);
+// }
+// }
+
+
+// public class array {
+
+//     public static void main(String[] args) {
+//         int arr[]={22,33,44,55,66,77,66,77,8,8,8};
+//         for(int i:arr)
+//         System.out.println(i);
+//     }
+// }
+
+// public class array {
+// public static void main(String[] args) {
+//     int [] a=new int[]{1,2,3,4,5,};
+//     for(int i:a)
+//     System.out.println(i);
+// }
+    
+// }
--- function.java
+++ function.java
@@ -61,25 +61,25 @@ import java.util.Scanner;
 
 
 
-public class function {
-public static void printfactorial(int n){
-    if(n<0){
+// public class function {
+// public static void printfactorial(int n){
+//     if(n<0){
 
-        System.out.println("invalid number");
-        return ;
-    }
-    int factorial =1;
-    for(int i=n;i>=1;i--){
-        factorial=factorial*i;
+//         System.out.println("invalid number");
+//         return ;
+//     }
+//     int factorial =1;
+//     for(int i=n;i>=1;i--){
+//         factorial=factorial*i;
 
-    }
-    System.out.println(factorial);
-    return;
-}
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        printfactorial(n);
+//     }
+//     System.out.println(factorial);
+//     return;
+// }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         printfactorial(n);
         
-    }
-}
+//     }
+// }
