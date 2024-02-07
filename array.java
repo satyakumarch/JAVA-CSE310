23f6130b1de0defe8091c588c5d89528d1b0bea0
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-02-07 08:29:27 +0530

    array of java frogram

---
+++ array.java
@@ -0,0 +1,16 @@
+public class array {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        a[0]=12;
+        a[1]=34;
+        a[2]=56;
+        a[3]=78;
+        a[4]=90;
+        System.out.println("lenght of the array is "+a.length);
+        System.out.println("printing the element of the array.");
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+        
+    }
+    
+}
