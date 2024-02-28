72073342ee8db5d3d6ef2a78a047396665bb5104
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-02-28 13:27:14 +0530

    adding paragraph

--- inheritance.java
+++ inheritance.java
@@ -1,19 +1,30 @@
-class firstprogram{
-    final double balance=500;
-    final void display(){
-    System.out.println("from the class A");
-    }
+// class firstprogram{
+//     final double balance=500;
+//     final void display(){
+//     System.out.println("from the class A");
+//     }
     
-}
-public class inheritance extends firstprogram{
-    // void display(){
-    //      System.out.println("ffrom class a first program");
-    // }
-    public static void main(String[] args){
-        //firstprogram r=new firstprogram();
-        inheritance r=new inheritance();
-        System.out.println("balance"+r.balance);
-        r.display();
-    }
+// }
+// public class inheritance extends firstprogram{
+//     // void display(){
+//     //      System.out.println("ffrom class a first program");
+//     // }
+//     public static void main(String[] args){
+//         //firstprogram r=new firstprogram();
+//         inheritance r=new inheritance();
+//         System.out.println("balance"+r.balance);
+//         r.display();
+//     }
 
-}
+// }
+
+The java instance of operator is ued to text weather the object is an instance of specific type
+(class or sub class or interface)
+abstract class and interface 
+rules for java abstract class
+1)any abstract class must be declared with  abstract keyword
+2)it can have anstract and non anstract method
+3)it can have constructor and static method
+4)it can have final method which will force the sub class not to changee the body of method.
+abstract method
+a method  which is declared  as abstract and doesnot have implementatation method
