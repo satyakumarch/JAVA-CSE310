81174ca42052e8930fa3f5cda3c663566a27de9e
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-11 21:01:50 +0530

    first commit

---
+++ Sample_String.java
@@ -0,0 +1,13 @@
+public class Sample_String {
+    public static void main(String[] args) {
+        //declare the String as an object S1 S2
+        String S1 = "Hello Java String Method";
+        String S2 = "RockStar";
+
+        //length() method of String returns the length of a String S1.
+        int length = S1.length();
+        System.out.println("Length of a String is: " + length);
+        //8 Length of a String RockStar
+        System.out.println("Length of a String is: " + S2.length());
+    }
+}
