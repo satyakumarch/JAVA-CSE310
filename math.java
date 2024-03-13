590d78e0dab8fff7c084d9946d67532630a818ef
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-13 11:46:46 +0530

    adding math function

--- math.java
+++ math.java
@@ -16,3 +16,18 @@
 //     }
 // }
 
+public class math{
+    public static void main(String args[]){
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.sqrt(16));
+
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(5.8));
+
+        System.out.println(Math.ceil(5.3));
+
+
+
+    }
+}
+
