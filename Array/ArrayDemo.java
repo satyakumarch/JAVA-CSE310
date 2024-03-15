00983dd466d71f98e5f754662a86f029be983940
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-15 12:30:10 +0530

    adding array code

---
+++ Array/ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo {
+	public static void main(String[] args) {
+		int[] ramanujanNumbers = { 1729, 4104, 13832, 20683, 32832 };
+		System.out.println("Printing Ramanujan Numbers: ");
+		for (int i = 0; i < ramanujanNumbers.length; i++) {
+			System.out.println(ramanujanNumbers[i]);
+		}
+	}
+}
