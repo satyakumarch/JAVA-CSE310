3af76d311f484f930799bf31b8db082100263341
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-29 22:53:41 +0530

    code to convert char into string

---
+++ codetantra/chartostring.java
@@ -0,0 +1,11 @@
+public class chartostring {
+    public static void main(String[] args) {
+      //input character variable 
+      char myChar = 'g';
+      //Using toString() method
+      //toString method take character parameter and convert string.
+      String myStr = Character.toString(myChar);
+      //print string value
+      System.out.println("String is: " + myStr);
+    }
+  } 
