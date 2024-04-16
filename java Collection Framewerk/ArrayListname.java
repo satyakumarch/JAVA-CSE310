7e1bde241f1366d499ec5d3cb94f9aa24404b817
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-04-16 12:05:04 +0530

    adding name of student in Arralist

---
+++ java Collection Framewerk/ArrayListname.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayListname{
+    public static void main(String[] args){
+        ArrayList <String> name=new ArrayList<String>();
+        name.add("mango");
+        name.add("banana");
+        name.add("gauva");
+        name.add("orange");
+        for(String element : name){
+        System.out.println("The fruits before removing:"+name);
+       
+    }
+}
+}
