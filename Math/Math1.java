12bc3b63485b27677bd8ca7fd7c15ac8d4ce1475
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-04-25 23:27:48 +0530

    MAth

---
+++ Math/Math1.java
@@ -0,0 +1,18 @@
+public class Math1{
+    public static void main(String args[]) {
+   
+     int i1 = 27;
+     int i2 = -45;
+     double d1 = 84.6;
+     double d2 = 0.45;
+     System.out.println("Absolute value of i1: " + Math.abs(i1));
+   
+     System.out.println("Absolute value of i2: " + Math.abs(i2));
+   
+     System.out.println("Absolute value of d1: " + Math.abs(d1));
+   
+     System.out.println("Absolute value of d2: " + Math.abs(d2));
+   
+    }
+   }
+   
---
+++ Math/mathceilfloor.java
@@ -0,0 +1,15 @@
+public class mathceilfloor {
+    public static void main(String args[]) {
+     double d1 = 84.6;
+     double d2 = 0.45;
+     System.out.println("Ceiling of '" + d1 + "' = " + Math.ceil(d1));
+   
+     System.out.println("Floor of '" + d1 + "' = " + Math.floor(d1));
+   
+     System.out.println("Ceiling of '" + d2 + "' = " + Math.ceil(d2));
+   
+     System.out.println("Floor of '" + d2 + "' = " + Math.floor(d2));
+   
+    }
+   }
+   
