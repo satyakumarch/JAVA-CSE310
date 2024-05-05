fdd2b4c0c24585a70232039d914093b541d3e3ac
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-05-05 11:24:13 +0530

    java code

---
+++ codetantra/ram.java
@@ -0,0 +1,13 @@
+public class ram{
+    public int sum(int n1,int n2){
+        return n1+n2;
+    }
+    public static void main(String[] args){
+        ram example =new ram();
+        int n1=40;
+        int n2=30;
+        int result=example.sum(n1,n2);
+        System.out.println("result "+result);
+    }
+    
+}
