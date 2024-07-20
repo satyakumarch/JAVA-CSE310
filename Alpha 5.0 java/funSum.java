dddd4a3cbc65ec5d3e2cd0c82c4770ec8c10f578
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 16:24:45 +0545

    swap of number

--- Alpha 5.0 java/funSum.java
+++ Alpha 5.0 java/funSum.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 public class funSum{
-    public static void sum(int a,int b){
+    public static void sum(int a,int b){//parameter or formal parameter
         int sum=a+b;
         System.out.println("The sum of " + a + " and " + b + " is " + sum);
 
+    }
+    public static void swap(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -11,8 +19,10 @@ public class funSum{
         int a=sc.nextInt();
         System.out.print("Enter the second number:");
         int b=sc.nextInt();
-        sum(a,b);
-        
+        // sum(a,b);//arguments or actual parameter
+        swap(a,b);
+
+       
 
     }
 }
