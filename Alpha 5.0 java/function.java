1dd44c3f9453ca68d7131986b11a49723db11bb2
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 15:54:38 +0545

    function starting

---
+++ Alpha 5.0 java/function.java
@@ -0,0 +1,11 @@
+
+public class function{
+    public static void printhelloworld(){
+        System.out.println("hello world welcome to you");
+        return;
+    }
+    public static void main(String[] args){
+        printhelloworld();//function call
+
+    }
+}
