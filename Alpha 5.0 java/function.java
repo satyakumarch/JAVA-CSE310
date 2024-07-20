0ca6ab07102d075b8f5d32376cfc46c16f24e0fd
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 16:52:21 +0545

    function in java

--- Alpha 5.0 java/function.java
+++ Alpha 5.0 java/function.java
@@ -1,11 +1,21 @@
 
 public class function{
+    //function to calculate sum of 2 nums
+    public static int sum(int a,int b){
+return a+b;
+    }
+    public static int sum(int a,int b,int c){
+        return a+b+c;
+
+    }
     public static void printhelloworld(){
         System.out.println("hello world welcome to you");
         return;
     }
     public static void main(String[] args){
         printhelloworld();//function call
+        System.out.println(sum(3,4));
+        System.out.println(sum(2,3,4));
 
     }
 }
