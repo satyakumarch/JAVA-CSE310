36054cce7ab726d0ed8efa9c5276341663e7144b
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 17:12:20 +0545

    prime number

---
+++ Alpha 5.0 java/prime.java
@@ -0,0 +1,15 @@
+public class prime{
+    public static boolean isPrime(int n){
+        boolean isPrime=true;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){//completely divide
+              return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(isPrime(3));
+
+    }
+}
