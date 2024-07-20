c72748c316dc06387f1cb5016d47a0c78c7732aa
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 17:19:34 +0545

    prime

--- Alpha 5.0 java/prime.java
+++ Alpha 5.0 java/prime.java
@@ -1,13 +1,26 @@
 public class prime{
     public static boolean isPrime(int n){
-        boolean isPrime=true;
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){//completely divide
-              return false;
+        if(n==2){
+            return true;
+
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
             }
+
         }
         return true;
     }
+    // public static boolean isPrime(int n){
+    //     boolean isPrime=true;
+    //     for(int i=2;i<=n-1;i++){
+    //         if(n%i==0){//completely divide
+    //           return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     public static void main(String[] args){
         System.out.println(isPrime(3));
 
