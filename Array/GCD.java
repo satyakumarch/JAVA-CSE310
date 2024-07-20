3f217be026be84615c2b472d11ddc74f31e3ef3d
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 19:11:23 +0545

    array

--- Array/GCD.java
+++ Array/GCD.java
@@ -4,7 +4,7 @@ public class GCD{
         int n1 = 366, n2 = 60;
         int hcf = hcf(n1, n2);
 
-        System.out.printf("G.C.D of %d and %d is %d.", n1, n2, hcf);
+        System.out.printf("G.C.D of %d and %d is : %d.", n1, n2, hcf);
     }
 
     public static int hcf(int n1, int n2)
