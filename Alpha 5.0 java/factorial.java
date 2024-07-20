a74f256440b26792f817f215d77cb5a1e8d7cfaf
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-20 16:31:38 +0545

    factorial in java

---
+++ Alpha 5.0 java/factorial.java
@@ -0,0 +1,15 @@
+public class factorial{
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1;i<=n;i++){
+            f=f*i;
+
+
+        }
+        return f;//factorial of n
+    }
+    public static void main(String[] args){
+        System.out.println(factorial(4));
+
+    }
+}
