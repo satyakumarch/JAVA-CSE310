fc3f76f15dc5e6a9ebae88d92af9187986dbbd28
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-21 17:00:36 +0545

    string palindrone

---
+++ Alpha 5.0 java/String/Palindrone.java
@@ -0,0 +1,16 @@
+public class Palindrone{
+    public static boolean isPalindrone(String str){
+        for(int i=0;i<str.length()/2;i++){
+            int n=str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        String str="racecar";
+        System.out.println(isPalindrone(str));
+    }
+}
