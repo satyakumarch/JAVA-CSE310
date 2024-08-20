c426cd15c976479b12dc79c09d86d606a6cef325
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-08-20 18:06:13 +0530

    update

--- codetantra/loops.java
+++ codetantra/loops.java
@@ -4,7 +4,7 @@ public class loops {
 
     public static void main(String[] args) {
         outerloop:
-        for(int i=20;i<=25;i++){
+        for(int i=2;i<=25;i++){
             System.out.println("i="+i);
             Innerloop:
             for(int j=1;j<10;j++){
