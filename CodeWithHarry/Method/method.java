b6abeca476f56ffef96017a08d50c9086556e028
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-10-10 01:36:20 +0530

    method

--- CodeWithHarry/Method/method.java
+++ CodeWithHarry/Method/method.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-public class method{
-    public static int grade(int mark){
-        if(mark>=90){
+public class method {
+    public static String grade(int mark) {
+        if (mark >= 90) {
             return "A++";
-        }else if(marks>=80){
+        } else if (mark >= 80) {
             return "A";
-        }else if(marks>=70)
-{
-return "B";
-}else if(marks>=60){
-    return "C";
-}
+        } else if (mark >= 70) {
+            return "B";
+        } else if (mark >= 60) {
+            return "C";
+        } else {
+            return "Fail";
+        }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter ypur marks");
-        int num=sc.nextInt();
-        System.out.println("Your grade is "+grade(num));
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks:");
+        int num = sc.nextInt();
+        System.out.println("Your grade is: " + grade(num));
     }
 }
