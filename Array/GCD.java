99ab9e408017c51d5814df20e6973b6d60c030b0
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-10-16 00:42:58 +0530

    practice

--- Array/GCD.java
+++ Array/GCD.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class GCD{
 
     public static void main(String[] args) {
-        int n1 = 366, n2 = 60;
-        int hcf = hcf(n1, n2);
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int hcf=hcf(n1,n2);
+        // int n1 = 366, n2 = 60;
+        // int hcf = hcf(n1, n2);
 
         System.out.printf("G.C.D of %d and %d is : %d.", n1, n2, hcf);
     }
