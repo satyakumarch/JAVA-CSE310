1ff81e1068b613ea872c3192f3d99fc64d3fc028
Satyakumarch <satyakumarchaudhary603@gmail.com>
2024-11-30 23:54:37 +0530

    runing code

--- Endterm/simpepg/SumOfDigit.java
+++ Endterm/simpepg/SumOfDigit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfDigit {++
+public class SumOfDigit {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
