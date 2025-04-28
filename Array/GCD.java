4dfb672c8517185b4c6f194d346df0191ec561e3
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-28 18:18:08 +0530

    java revision

--- Array/GCD.java
+++ Array/GCD.java
@@ -1,22 +1,50 @@
+// import java.util.Scanner;
+
+// public class GCD {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the first number: ");
+//         int n1 = sc.nextInt();
+
+//         System.out.print("Enter the second number: ");
+//         int n2 = sc.nextInt();
+
+//         int hcf = hcf(n1, n2);
+
+//         // Corrected printf statement with format specifiers
+//         System.out.printf("G.C.D of %d and %d is %d.%n", n1, n2, hcf);
+
+//         sc.close(); // Close the scanner40
+//     }
+
+//     public static int hcf(int n1, int n2) {
+//         if (n2 != 0)
+//             return hcf(n2, n1 % n2);
+//         else
+//             return n1;
+//     }
+// }
 import java.util.Scanner;
 public class GCD{
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number:");
         int n1=sc.nextInt();
+        System.out.print("Enter second number:");
         int n2=sc.nextInt();
+        
         int hcf=hcf(n1,n2);
-        // int n1 = 366, n2 = 60;
-        // int hcf = hcf(n1, n2);
+        System.out.printf("The GCD of the %d and %d is %d.%n ",n1,n2,hcf);
 
-        System.out.printf("G.C.D of %d and %d is : %d.", n1, n2, hcf);
+
     }
-
-    public static int hcf(int n1, int n2)
-    {
-        if (n2 != 0)
-            return hcf(n2, n1 % n2);
-        else
+    public static int hcf(int n1,int n2){
+        if(n2!=0){
+            return hcf(n2,n1 % n2);
+        }else{
             return n1;
+        }
     }
 }
--- Array/Power.java
+++ Array/Power.java
@@ -1,20 +1,40 @@
-class Power {
-    public static void main(String[] args) {
+// class Power {
+//     public static void main(String[] args) {
         
-      int base = 3, powerRaised = 4;
-      int result = power(base, powerRaised);
+//       int base = 3, powerRaised = 4;
+//       int result = power(base, powerRaised);
   
-      System.out.println(base + "^" + powerRaised + "=" + result);
-    }
+//       System.out.println(base + "^" + powerRaised + "=" + result);
+//     }
   
-    public static int power(int base, int powerRaised) {
-      if (powerRaised != 0) {
+//     public static int power(int base, int powerRaised) {
+//       if (powerRaised != 0) {
   
-        // recursive call to power()
-        return (base * power(base, powerRaised - 1));
-      }
-      else {
-        return 1;
-      }
+//         // recursive call to power()
+//         return (base * power(base, powerRaised - 1));
+//       }
+//       else {
+//         return 1;
+//       }
+//     }
+//   }
+ import java.util.Scanner;
+ public class Power{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Ente base number:");
+    int base=sc.nextInt();
+    System.out.print("Enter powerRaised number:");
+    int powerRaised=sc.nextInt();
+
+    int result=power(base,powerRaised);
+    System.out.println(base+"^"+powerRaised+"="+result);
+  }
+  public static int power(int base,int powerRaised){
+    if(powerRaised!=0){
+      return (base*power(base,powerRaised-1));
+    }else{
+      return -1;
     }
   }
+ }
