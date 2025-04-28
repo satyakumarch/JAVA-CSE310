fd3dc184dd65b56fa9aafd21e8547316fb647e4e
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-28 18:33:48 +0530

    java revision

--- Array/sum.java
+++ Array/sum.java
@@ -1,11 +1,27 @@
+// class arrayexample{
+//     void sumofarray(){
+//         int[] arr={1,2,3,4,5};
+//         int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             sum=sum+arr[i];
+//         }
+//         System.out.println("The sum of the array element is : "+sum);
+//     }
+// }
+// public class sum{
+//     public static void main(String[] args){
+//         arrayexample obj=new arrayexample();
+//         obj.sumofarray();
+//     }
+// }
 class arrayexample{
     void sumofarray(){
-        int[] arr={1,2,3,4,5};
+        int arr[]={1,2,3,4,5,6};
         int sum=0;
         for(int i=0;i<arr.length;i++){
             sum=sum+arr[i];
         }
-        System.out.println(sum);
+        System.out.print("The sum of the array element is:"+sum);
     }
 }
 public class sum{
