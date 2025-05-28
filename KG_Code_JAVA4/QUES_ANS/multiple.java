5e787ef71f782dfd656f06783fecf7c42f220819
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-05-28 08:50:28 +0545

    multiplication of two float number

---
+++ KG_Code_JAVA4/QUES_ANS/multiple.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class multiple{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number a:");
+        float a=sc.nextFloat();
+        System.out.print("Enter the number b:");
+        float b=sc.nextFloat();
+        System.out.println("The multiplication of "+a+" and "+b+" is :"+(a*b));
+
+
+    }
+}
