23976d0becaab9308e2ae0701b4d7e9546703094
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-06-07 21:57:54 +0545

    array in java

--- KG_Code_JAVA4/QUES_ANS/Array.java
+++ KG_Code_JAVA4/QUES_ANS/Array.java
@@ -1,12 +1,31 @@
+// public class Array {
+//     public static void main(String[] args) {
+//         int[][] original = {
+//             {1, 2, 3},
+//             {4, 5, 6},
+//             {3,4,6},
+
+//         };
+
+//         int[][] clone = original.clone();
+
+//         System.out.println("Original[0] == Clone[0]? " + (original[0] == clone[0]));
+//     }
+// }
+
 public class Array {
     public static void main(String[] args) {
-        int[][] original = {
-            {1, 2, 3},
-            {4, 5, 6}
-        };
+        int[] numbers = {5, 10, 15, 20, 25};
 
-        int[][] clone = original.clone();
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
 
-        System.out.println("Original[0] == Clone[0]? " + (original[0] == clone[0]));
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println("Sum of array elements: " + sum);
     }
 }
